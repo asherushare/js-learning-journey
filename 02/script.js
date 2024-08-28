43963d05151587f3502038c74df55e2544da0d6f
asherushare <asherushare@gmail.com>
2024-08-28 11:51:52 +0530

    Second commit

--- 02/script.js
+++ 02/script.js
@@ -15,3 +15,37 @@ if(true) {
 
 console.log(age);
 console.log(id);
+
+let num = 22.3;
+let num2 = 22.3;
+
+console.log(num + num2);
+
+let fName = "Mettle";
+let Lname = "Patra";
+
+console.log(fName + " " + Lname);
+
+// Boolean
+
+let isLoginDate = false;
+
+let lastLogin = null;
+
+console.log(lastLogin + 12);
+
+//Object 
+
+const persion = {
+    firstName: "Mettle",
+    age: 23,
+    isLogin: false
+}
+
+console.log(persion);
+console.log(persion.firstName);
+
+console.log('333' + 22);
+console.log('233' * 12);
+
+console.log(typeof('23' + 33));
